b4d5b3ae4700a5614b729fe05142876751da31f2
samilieberman <samilieberman1@gmail.com>
2022-10-06 21:10:59 -0400

    Initialize project using Create React App
    add contributing.md
    
    add hello world message
    
    Add new text and gator image
    
    Updated my name is to Rob Mack
    
    1.0.0

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import gatorLogo from './gator.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Rob Mack and this is my project </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
